80e56db237442112b53d6982cd2da1836d5cb336
NidhoggDJoking <250348426@qq.com>
2019-07-14 21:58:28 +0800

    add

---
+++ vueone/src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+<li @click="del">{{com}}</li>
+</template>
+
+<script>
+export default{
+        // props:["com"]
+         props: {
+            com: String,
+            index:String,
+        },
+        methods:{
+            del() {
+               this.$emit('delete',this.index)
+            }
+        }
+}
+</script>
+
---
+++ vueone/src/views/Test.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <input v-model="inputVaule"/>
+        <button @click="submit">提交</button>
+        <ul>
+            <todoitm 
+                v-for="(item,index) of list"
+                :com="item"
+                v-bind:key="index"
+                :index="indexs"
+                @delete="hand"
+                ></todoitm>
+        </ul>
+    </div>
+</template>
+
+<script>
+import TodoList from '../components/TodoList' 
+export default {
+    components:{
+      'todoitm':TodoList,
+    },
+    data (){
+        return{
+            inputVaule:'',
+            list:[]
+        }
+    },
+    methods:{
+        submit() {
+           this.list.push(this.inputVaule);
+           this.inputVaule=''
+        },
+        hand(index){
+            this.list.splice(index,1);
+        }
+    }
+}
+</script>
+
