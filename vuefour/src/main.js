2fe3ec06a2b12b6a2ea8f09d46f4ee55a4facc92
joking <250348426@qq.com>
2019-07-27 10:46:44 +0800

    tj

--- vuefour/src/main.js
+++ vuefour/src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './css/base.css';
-
+// vue-resource` import should occur before import of `./App.vue`
 Vue.use(VueResourse);
 Vue.config.productionTip = false;
 new Vue({
