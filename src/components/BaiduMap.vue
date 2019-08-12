770cd5129349d30367159710d51af259ba56c6db
joking <250348426@qq.com>
2019-08-12 15:19:40 +0800

    tj

--- src/components/BaiduMap.vue
+++ src/components/BaiduMap.vue
@@ -1,5 +1,5 @@
 <template>
- <baidu-map class="bm-view" v-bind="mapOptions">
+ <baidu-map class="bm-view" v-bind="mapOptions" @ready="handler">
  	  <bm-control :offset="{width: '10px', height: '10px'}">
 	    <!-- <bm-auto-complete v-model="keyword" :sugStyle="{zIndex: 1}">
 	   	    </bm-auto-complete> -->
@@ -25,7 +25,8 @@ export default {
 	   return {
 	     mapOptions: {
 	       ak: 'Z7YUjqYw69iFb7wFNc4LAbHSvp4e9a1U',
-	       center: '北京',
+		   center: '北京',
+		   // center: {lng: 0, lat: 0},
 		   scrollWheelZoom: true,
 		   zoom:13,
 	     },
@@ -33,7 +34,23 @@ export default {
 	   }
 	},
 	methods: {
-	
+		handler ({BMap, map}) {
+			var geolocation = new BMap.Geolocation();
+			geolocation.getCurrentPosition(function(r){
+				if(this.getStatus() == BMAP_STATUS_SUCCESS){
+					var marker = new BMap.Marker(r.point);
+					map.addOverlay(marker);
+					marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+					map.panTo(r.point);
+					
+					// alert('您的位置：'+r.point.lng+','+r.point.lat);
+				}
+				else {
+					console.error('百度:failed'+this.getStatus());
+				}        
+			},{enableHighAccuracy: true})
+      
+    	}
 	}
 }
 </script>
