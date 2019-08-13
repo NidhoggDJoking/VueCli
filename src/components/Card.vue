e78ab5aa239b9d3c3d82679ba6585b53e6bfbf1e
joking <250348426@qq.com>
2019-08-13 17:15:44 +0800

    字体调整

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -29,7 +29,7 @@
 		  <div class="card-infos">
 			<h2 class="card__title">模块 1</h2>
 			<p class="card__text">
-			  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, eius, asperiores. Incidunt sapiente est quae iure...
+			  可人不是断气的时候才真的死了。有人说人会死三次，第一次是他断气的时候 ，从生物学上他死了；第二次是他下葬的时候，人们来参加他的葬礼，怀念他的一生，然后在社会上他死了，不再会有他的位置；第三次是最后一个记得他的人把他忘记的时候，那时候他才真正的死了...
 			</p>
 		  </div>
 		</div>
@@ -40,7 +40,7 @@
 		  <div class="card-infos">
 			<h2 class="card__title">模块 2</h2>
 			<p class="card__text">
-			  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum vitae necessitatibus, dolorem similique vero explicabo...
+			  一个人被彻底忘记，到底需要多长时间？我想这应该取决于这个人的知名度吧。我突然明白了为什么有些人宁愿遭世人唾骂也要遗臭万年了，因为那样至少还有人记得你，哪怕是以鄙夷的眼光...
 			</p>
 		  </div>
 		</div>
@@ -51,7 +51,7 @@
 		  <div class="card-infos">
 			<h2 class="card__title">模块 3</h2>
 			<p class="card__text">
-			  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, placeat voluptate, fuga tenetur eos ducimus animi porro...
+			  如果喜欢谁，就满世界去找她，别等她来找你，她可能也在等你……别让她等得对你失望了。如果你喜欢的人要嫁人了，就跟她表白一下，就算为此要把她婚车的车胎打爆也没什么，这是你说出来的最后机会。把这个秘密带进棺材没价值，连陪葬品都算不上...
 			</p>
 		  </div>
 		</div>
@@ -62,7 +62,7 @@
 		  <div class="card-infos">
 			<h2 class="card__title">模块 4</h2>
 			<p class="card__text">
-			  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit doloremque quisquam, obcaecati unde nam est quos...
+			  每个人的一生都会遇见某些人，喜欢上她。有些人在合适的时间里遇到，就像是在春天遇到花开，所以一切都很好，他们会相恋、订婚、结婚、一起生活。而有的人在错误的时间遇到 ，就像是在冬天，隔着冰看见浮上来换气的鱼，所以只能看着，鱼换完气，沉到水下去，就看不见了，再也没有后续。但是我们能说在春天遇到花是对的，而在冬天遇到鱼是错的么？在错误的时间里遇到，就能克制自己不喜欢那个人么？是不是仍然会用尽了力气想去接近，想尽办法掩饰自己，甚至伪装成另外一条鱼...
 			</p>
 		  </div>
 		</div>
@@ -73,7 +73,7 @@
 		  <div class="card-infos">
 			<h2 class="card__title">模块 5</h2>
 			<p class="card__text">
-			  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse corporis hic minima nisi reprehenderit...
+			  在这个世界的某个角落，必有为你而生的人，当你站在悬崖尽头时不要失去希望，要多坚持那么一秒钟，等那个人一骑绝尘如狂风闪电般出现在你面前，你将跨上他的马背，即便他是被所罗门囚禁了一千年的魔鬼...
 			</p>
 		  </div>
 		</div>
@@ -84,7 +84,7 @@
 		  <div class="card-infos">
 			<h2 class="card__title">模块 6</h2>
 			<p class="card__text">
-			  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel voluptatibus, id, deserunt inventore...
+			  你最爱的人，你为他做了很多事，但他不知道，因为你觉得做这些事都是应该的，你忘记跟他说了...
 			</p>
 		  </div>
 		</div>
@@ -95,7 +95,7 @@
 		  <div class="card-infos">
 			<h2 class="card__title">模块 7</h2>
 			<p class="card__text">
-			  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum enim beatae vero culpa, fuga, magni sunt dolores nam...
+			  愿你我江湖永别后会无期，道别的话多说无益，祝你无边落木萧萧下、西出阳关无故人、雨淋白骨血染草、月冷黄沙鬼守尸...
 			</p>
 		  </div>
 		</div>
@@ -106,7 +106,7 @@
 		  <div class="card-infos">
 			<h2 class="card__title">模块 8</h2>
 			<p class="card__text">
-			  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae numquam, blanditiis necessitatibus...
+			  世间一切幸福，皆月影一现的昙花，唯有孤独与痛，常伴在黄昏深处...
 			</p>
 		  </div>
 		</div>
@@ -117,7 +117,7 @@
 		  <div class="card-infos">
 			<h2 class="card__title">模块 9</h2>
 			<p class="card__text">
-			  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur adipisci voluptatum laborum officiis...
+			  喜欢一个人那么久，那个人就和自己的过去捆在一起了，要是后悔以前喜欢过谁，不就是把自己以前的时间都否定了么？
 			</p>
 		  </div>
 		</div>
@@ -209,13 +209,13 @@ hr{
 }
 .card__title{
   font-family: 'Hind';
-  font-size: 1.8rem;
+  font-size: 1.5rem;
   font-weight: 600;
   line-height: 1.4;
   margin: 0 0 10px;
 }
 .card__text{
-  font-size: 1.4rem;
+  font-size: 1.2rem;
   font-weight: 300;
   margin: 0;
   color: #86888A;
